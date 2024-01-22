4073143c953bcce588e57d2f8b5e630ac8d1e7e1
Alex Mangust <sanya.stogny@gmail.com>
2024-01-23 00:56:48 +0800

    Промежуточная атестация. Выполнил задание (Добавил комментарий в класс App)

--- IntermediateCertification/ComplexNumberCalculator/src/App.java
+++ IntermediateCertification/ComplexNumberCalculator/src/App.java
@@ -6,9 +6,9 @@ import Views.ViewCalculator;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Decorator decorator = new Decorator(new CalculatorComplexNumber(), new LoggerFile());
-        ControllerCalculator calculator = new ControllerCalculator(decorator, new ViewCalculator());
-        calculator.run();
+        Decorator decorator = new Decorator(new CalculatorComplexNumber(), new LoggerFile()); // Создаю экземпляр класса Decorator, в котором, в качестве аргументов, выступают экземпляр класса CalculatorComplexNumber и экземпляр класса LoggerFile
+        ControllerCalculator calculator = new ControllerCalculator(decorator, new ViewCalculator()); // Создаю экземпляр класса ControllerCalculartir, в котором, в качестве аргумента, выступают экземпляр класса Decorator и экземпляр класса ViewCalculator
+        calculator.run(); // Вызываю функцию для запуска программы
         
     }
 }
